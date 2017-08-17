f89199c31f8500a499d940f955326ea164e31f58
BachHealth <bachhealth@Spencers-MacBook-Pro.local>
2017-08-17 14:45:58 -0600

    getting ready for deployment

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let cors = require("cors");
 let mongoose = require("mongoose");
+let path = require("path");
+let settings = require("./config/settings.js");
 
 let BarRouter = require("./routes/bar-route.js");
 let MenuRouter = require("./routes/menuItem-route.js");
 let OrderRouter = require("./routes/order-route.js");
 
-mongoose.connect("mongodb://localhost:27017/barApp");
+mongoose.connect(settings.db);
 
 let app = express();
 
@@ -21,6 +23,12 @@ app.use(cors());
 app.use("/bar", BarRouter);
 app.use("/order", OrderRouter);
 app.use("/menu", MenuRouter);
+
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"))
+});
 
 app.listen(PORT, () => {
     console.log(`starting server on port ${PORT}`)
